b622ad2470183962e66ff6503ef5d44d39e7f7bb
Zubair siddiqui <zubairoist@gmail.com>
2026-09-17 13:59:03 +0530

    Update grade_checker.py

--- Python-and-bash/grade_checker.py
+++ Python-and-bash/grade_checker.py
@@ -1,1 +1,12 @@
-
+score = int(input("Enter your score: "))
+if score >= 90:
+    grade = "A"
+elif score >= 80:
+    grade = "B"
+elif score >= 70:
+    grade = "C"
+elif score >= 60:
+    grade = "D"
+else:
+    grade = "F"
+print("Grade:", grade)
