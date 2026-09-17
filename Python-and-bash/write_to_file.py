99a7eda4ef079e86f096542dd6a6687984ffbba3
Zubair siddiqui <zubairoist@gmail.com>
2026-09-17 15:24:52 +0530

    Create write_to_file.py

---
+++ Python-and-bash/write_to_file.py
@@ -0,0 +1,9 @@
+file = open("sample.txt", "w")
+
+file.write("Hello, this is my Python and bash assignment.\n")
+file.write("Here I am writing data to a file using Python.\n")
+file.write("This file was created using the open() and write() functions.\n")
+
+file.close()
+
+print("Data written to sample.txt successfully.")
