95faef5de17389473a955427cf702564d427313b
Zubair siddiqui <zubairoist@gmail.com>
2026-09-18 22:40:12 +0530

    Update app.py

--- Flask-and-MongoDB/app.py
+++ Flask-and-MongoDB/app.py
@@ -1,1 +1,16 @@
+from flask import Flask, jsonify
+import json
 
+app = Flask(__name__)
+
+
+@app.route("/api")
+def api():
+    with open("data.json", "r") as file:
+        data = json.load(file)
+
+    return jsonify(data)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
