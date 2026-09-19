5c256c94a83244dff25d4e9e7ff7b3cf8243ad90
Zubair siddiqui <zubairoist@gmail.com>
2026-09-19 15:51:49 +0530

    Update write_to_file.py

--- Python-and-bash/write_to_file.py
+++ Python-and-bash/write_to_file.py
@@ -1,9 +1,10 @@
+# This program demonstrates how to write data to a file using Python. It creates a new file called "sample.txt" and writes multiple lines of text into it.
 file = open("sample.txt", "w")
-
+# Write data to the file
 file.write("Hello, this is my Python and bash assignment.\n")
 file.write("Here I am writing data to a file using Python.\n")
 file.write("This file was created using the open() and write() functions.\n")
-
+# Close the file after writing
 file.close()
-
+# Print a message indicating that the data has been written successfully
 print("Data written to sample.txt successfully.")
