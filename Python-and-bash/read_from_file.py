e6447ad9721d15623ee2bf30988a38ba3d16d215
Zubair siddiqui <zubairoist@gmail.com>
2026-09-19 15:52:47 +0530

    Update read_from_file.py

--- Python-and-bash/read_from_file.py
+++ Python-and-bash/read_from_file.py
@@ -1,8 +1,9 @@
+# This program demonstrates how to read data from a file using Python. It opens an existing file called "sample.txt" and reads its contents, displaying them on the console.
 file = open("sample.txt", "r")
-
+# Read data from the file
 content = file.read()
-
+# Print the contents of the file
 print("File contents:")
 print(content)
-
+# Close the file after reading
 file.close()
